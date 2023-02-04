1e51cb3ee3e4ba652bc41d203c7bbd84bfd6aa7e
genabdulrehman <76816147+genabdulrehman@users.noreply.github.com>
2023-02-04 14:22:42 +0500

    Initial Commit

---
+++ transactions/transactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  transactionsApp.swift
+//  transactions
+//
+//  Created by Abdul Rehman on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct transactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
